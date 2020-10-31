1c292e6629de9af1ce416e4658bb80e46d8aa961
Asamin <66409874+Asamin-git-user@users.noreply.github.com>
2020-11-01 00:16:41 +0900

    Python練習用

---
+++ python-test/test.py
@@ -0,0 +1,35 @@
+# コメント
+
+print("Hello world!")
+print("test")
+
+# 演算
+print(1+1)
+print(1-1)
+print(2*2)
+print(10/2)
+print(5%3)
+
+
+# 変数
+word1 = "Hello World!"
+print(word1)
+print(type(word1))
+print(len(word1))
+
+suuji = 0
+print(suuji)
+print(type(suuji))
+
+
+# 条件分岐と関係演算子
+if suuji == 5 :
+    print("True")
+elif suuji >= 5 :
+    print("False")
+else :
+    print("none")
+
+
+
+# 関数
